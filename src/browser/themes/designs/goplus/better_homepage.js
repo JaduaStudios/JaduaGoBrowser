fa9d5e5ffaad27a7660731fd37fe0611f9f12103
Josh Ben-Nathan <48727939+Jadua1010@users.noreply.github.com>
2024-01-31 13:28:25 +0100

    Update better_homepage.js

--- src/browser/themes/designs/goplus/better_homepage.js
+++ src/browser/themes/designs/goplus/better_homepage.js
@@ -1,12 +1,16 @@
-  // Check your condition here
-  var shouldApplyBackground = Services.prefs.getStringPref("browser.newtabpage.activity-stream.go.background.image"); // Change this based on your actual condition
+const { Prefs } = ChromeUtils.import(
+  "resource://activity-stream/lib/ActivityStreamPrefs.jsm"
+);
 
-  // Get the #root element
-  var rootElement = document.getElementById('root');
+// Check your condition here
+var shouldApplyBackground = Services.prefs.getStringPref("browser.newtabpage.activity-stream.go.background.image"); // Change this based on your actual condition
 
-  // Check the condition and add the background styles if true
-  if (shouldApplyBackground) {
-    rootElement.style.background = 'linear-gradient(rgba(211, 211, 211, 0.31), rgba(93, 93, 96, 0.5)), url(https://source.unsplash.com/random/3840x2160/?city+day,landscape+day,abstract+day)';
-    rootElement.style.backgroundSize = 'cover';
-    // Add more styles as needed
-  }
+// Get the #root element
+var rootElement = document.getElementById('root');
+
+// Check the condition and add the background styles if true
+if (shouldApplyBackground) {
+  rootElement.style.background = 'linear-gradient(rgba(211, 211, 211, 0.31), rgba(93, 93, 96, 0.5)), url(https://source.unsplash.com/random/3840x2160/?city+day,landscape+day,abstract+day)';
+  rootElement.style.backgroundSize = 'cover';
+  // Add more styles as needed
+}
