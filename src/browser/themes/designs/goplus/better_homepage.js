3fd94f9edaeb5839444eb655f8e37bd02d08f227
Jadua1010 <jmb-n@outlook.com>
2024-02-01 01:50:38 +0100

    Newtab updates

--- src/browser/themes/designs/goplus/better_homepage.js
+++ src/browser/themes/designs/goplus/better_homepage.js
@@ -1,16 +1,33 @@
-const { Prefs } = ChromeUtils.import(
-  "resource://activity-stream/lib/ActivityStreamPrefs.jsm"
-);
+document.addEventListener('DOMContentLoaded', function () {
+  // Adding a delay (in milliseconds) to ensure styles are applied after DOM load
+  setTimeout(function () {
+    // Get the #root element
+    var rootElement = document.getElementById('root');
 
-// Check your condition here
-var shouldApplyBackground = Services.prefs.getStringPref("browser.newtabpage.activity-stream.go.background.image"); // Change this based on your actual condition
+    // Check if --newtab-background-color is rgb(242, 241, 244)
+    var newTabBackgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--newtab-background-color').trim();
 
-// Get the #root element
-var rootElement = document.getElementById('root');
+    console.log('Current background color:', newTabBackgroundColor);
 
-// Check the condition and add the background styles if true
-if (shouldApplyBackground) {
-  rootElement.style.background = 'linear-gradient(rgba(211, 211, 211, 0.31), rgba(93, 93, 96, 0.5)), url(https://source.unsplash.com/random/3840x2160/?city+day,landscape+day,abstract+day)';
-  rootElement.style.backgroundSize = 'cover';
-  // Add more styles as needed
-}
+    if (newTabBackgroundColor === 'rgb(29, 33, 41)') {
+      // Add different background styles if the condition is true for the new color
+      rootElement.style.background = 'linear-gradient(rgba(34, 36, 43, 0.5), rgba(17, 18, 19, 0.5)), url(https://source.unsplash.com/random/3840x2160/?city night,landscape night,dark)';
+	  rootElement.style.backgroundSize = 'cover';
+    } else if (newTabBackgroundColor === 'rgb(242, 241, 245)') {
+      // Add the background styles if the condition is true
+      rootElement.style.background = 'linear-gradient(rgba(255, 255, 255, 0.31), rgba(255, 255, 255, 0.09)), url("https://source.unsplash.com/random/3840x2160/?city white,landscape day,pastel colors, light")';
+	  rootElement.style.backgroundSize = 'cover';
+	} else if (newTabBackgroundColor === 'rgb(5, 7, 10)') {
+      // Add the background styles if the condition is true
+      rootElement.style.background = 'url(chrome://browser/skin/gx/images/newtab/wallpaper-dark.png)';
+	  rootElement.style.backgroundSize = 'cover';
+	} else if (newTabBackgroundColor === 'rgb(243, 241, 244)') {
+      // Add the background styles if the condition is true
+      rootElement.style.background = 'url(chrome://browser/skin/gx/images/newtab/wallpaper-light.png)';
+	  rootElement.style.backgroundSize = 'cover';
+    } else {
+      console.log('No matching background color found');
+    }
+	
+  }, 500); // Adding a 500 ms (0.5 second) delay
+});
