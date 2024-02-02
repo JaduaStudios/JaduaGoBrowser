dc11724b53d6bb009c16d6aedefbe8d7fb165f70
Jadua1010 <jmb-n@outlook.com>
2024-02-02 02:10:37 +0100

    Fixed syntax

--- src/browser/themes/designs/goplus/better_homepage.js
+++ src/browser/themes/designs/goplus/better_homepage.js
@@ -11,11 +11,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (newTabBackgroundColor === 'rgb(29, 33, 41)') {
       // Add different background styles if the condition is true for the new color
-      rootElement.style.background = 'linear-gradient(rgba(34, 36, 43, 0.5), rgba(17, 18, 19, 0.5)), url(https://source.unsplash.com/random/3840x2160/?city night,landscape night,dark)';
+      rootElement.style.background = 'linear-gradient(rgba(34, 36, 43, 0.5), rgba(17, 18, 19, 0.5)), url("https://source.unsplash.com/random/3840x2160/?city night,landscape night,dark")';
 	  rootElement.style.backgroundSize = 'cover';
     } else if (newTabBackgroundColor === 'rgb(242, 241, 245)') {
       // Add the background styles if the condition is true
-      rootElement.style.background = 'linear-gradient(rgba(255, 255, 255, 0.31), rgba(255, 255, 255, 0.09)), url("https://source.unsplash.com/random/3840x2160/?city white,landscape day,pastel colors, light")';
+      rootElement.style.background = 'linear-gradient(rgba(255, 255, 255, 0.31), rgba(255, 255, 255, 0.09)), url("https://source.unsplash.com/random/3840x2160/?city white,landscape day,pastel colors,light")';
 	  rootElement.style.backgroundSize = 'cover';
 	} else if (newTabBackgroundColor === 'rgb(5, 7, 10)') {
       // Add the background styles if the condition is true
